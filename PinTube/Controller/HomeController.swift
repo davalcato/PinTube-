02af25f86bbf687f2da636886ac79ade9ba93de3
davalcato <davalcato@yahoo.com>
2019-03-15 21:36:07 -0700

    Added the ApiService file to clean up the code.

--- PinTube/Controller/HomeController.swift
+++ PinTube/Controller/HomeController.swift
@@ -14,51 +14,11 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     
     
     func fetchVideos() {
-        let url = NSURL(string: "https://s3-us-west-2.amazonaws.com/youtubeassets/home.json")
-        URLSession.shared.dataTask(with: url! as URL) { (data, respones, error) in
+        ApiService.sharedInstance.fetchVideos { (videos: [Video]) in
             
-            if error != nil {
-                print(error as Any)
-                return
-                
-            }
-            
-            do {
-                
-                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
-                
-                self.videos = [Video]()
-                
-                for dictionary in json as! [[String: AnyObject]] {
-                    
-                    let video = Video()
-                    video.title = dictionary["title"] as? String
-                    video.thumbnailImageName = dictionary["thumbnail_image_name"] as? String
-                    
-                    let channelDictionary = dictionary["channel"] as! [String: AnyObject]
-                    
-                    let channel = Channel()
-                    channel.name = channelDictionary["name"] as? String
-                    channel.profileImageName = channelDictionary["profile_image_name"] as? String
-                    
-                    video.channel = channel
-                    
-                    self.videos?.append(video)
-                    
-                }
-                
-                DispatchQueue.main.async {
-                    self.collectionView?.reloadData()
-                    
-                }
-                
-            } catch let jsonError {
-                print(jsonError)
-                
-            }
-            
-            }.resume()
-        
+            self.videos = videos
+            self.collectionView.reloadData()
+        }
     }
     
     override func viewDidLoad() {
