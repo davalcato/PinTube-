14b5750c823724beede2d414589ba13822fd80a4
davalcato <davalcato@yahoo.com>
2019-03-18 19:01:33 -0700

    Introduce the fetchTrendingFeed jSon

--- PinTube/ApiService/ApiService.swift
+++ PinTube/ApiService/ApiService.swift
@@ -60,5 +60,53 @@ class ApiService: NSObject {
         
     }
     
+    func fetchTrendingFeed(completion: @escaping ([Video]) ->()) {
+        let url = NSURL(string: "https://s3-us-west-2.amazonaws.com/youtubeassets/trending.json")
+        URLSession.shared.dataTask(with: url! as URL) { (data, respones, error) in
+            
+            if error != nil {
+                print(error as Any)
+                return
+                
+            }
+            
+            do {
+                
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                
+                var videos = [Video]()
+                
+                for dictionary in json as! [[String: AnyObject]] {
+                    
+                    let video = Video()
+                    video.title = dictionary["title"] as? String
+                    video.thumbnailImageName = dictionary["thumbnail_image_name"] as? String
+                    
+                    let channelDictionary = dictionary["channel"] as! [String: AnyObject]
+                    
+                    let channel = Channel()
+                    channel.name = channelDictionary["name"] as? String
+                    channel.profileImageName = channelDictionary["profile_image_name"] as? String
+                    
+                    video.channel = channel
+                    
+                    videos.append(video)
+                    
+                }
+                
+                DispatchQueue.main.async {
+                    completion(videos)
+                    
+                }
+                
+            } catch let jsonError {
+                print(jsonError)
+                
+            }
+            
+            }.resume()
+        
+    }
+    
 
 }
--- PinTube/View/TrendingCell.swift
+++ PinTube/View/TrendingCell.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class TrendingCell: FeedCell {
     
-    
-    
+    override func fetchVideos() {
+        ApiService.sharedInstance.fetchTrendingFeed { (videos) in
+            self.videos = videos
+            self.collectionView.reloadData()
+        }
+        
+    }
     
 }
