e06ce2445ee84a8e218c98e873d5c6c89d5ea162
davalcato <davalcato@yahoo.com>
2019-03-20 18:45:26 -0700

    Removed all code duplication in terms of Json

--- PinTube/ApiService/ApiService.swift
+++ PinTube/ApiService/ApiService.swift
@@ -13,50 +13,10 @@ class ApiService: NSObject {
     static let sharedInstance = ApiService()
     
     func fetchVideos(completion: @escaping ([Video]) ->()) {
-        let url = NSURL(string: "https://s3-us-west-2.amazonaws.com/youtubeassets/home.json")
-        URLSession.shared.dataTask(with: url! as URL) { (data, respones, error) in
+        fetchFeedForUrlString(urlString: "https://s3-us-west-2.amazonaws.com/youtubeassets/home.json") { (videos) in
+            completion(videos)
             
-            if error != nil {
-                print(error as Any)
-                return
-                
-            }
-            
-            do {
-                
-                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
-                
-                var videos = [Video]()
-                
-                for dictionary in json as! [[String: AnyObject]] {
-                    
-                    let video = Video()
-                    video.title = dictionary["title"] as? String
-                    video.thumbnailImageName = dictionary["thumbnail_image_name"] as? String
-                    
-                    let channelDictionary = dictionary["channel"] as! [String: AnyObject]
-                    
-                    let channel = Channel()
-                    channel.name = channelDictionary["name"] as? String
-                    channel.profileImageName = channelDictionary["profile_image_name"] as? String
-                    
-                    video.channel = channel
-                    
-                    videos.append(video)
-                    
-                }
-                
-                DispatchQueue.main.async {
-                  completion(videos)
-                    
-                }
-                
-            } catch let jsonError {
-                print(jsonError)
-                
-            }
-            
-            }.resume()
+        }
         
     }
     
