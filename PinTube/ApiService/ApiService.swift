14a5c13ae6e8b62f02ddd791b725b2c6b59930ab
davalcato <davalcato@yahoo.com>
2019-03-20 20:12:06 -0700

    Added the numberOfViews to the ApiService file

--- PinTube/ApiService/ApiService.swift
+++ PinTube/ApiService/ApiService.swift
@@ -54,6 +54,8 @@ class ApiService: NSObject {
                     video.title = dictionary["title"] as? String
                     video.thumbnailImageName = dictionary["thumbnail_image_name"] as? String
                     
+                    video.numberOfViews = dictionary["number_of_views"] as? NSNumber
+                    
                     let channelDictionary = dictionary["channel"] as! [String: AnyObject]
                     
                     let channel = Channel()
@@ -80,5 +82,32 @@ class ApiService: NSObject {
         }
     
     }
+
+//let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+//
+//var videos = [Video]()
+//
+//for dictionary in json as! [[String: AnyObject]] {
+//
+//    let video = Video()
+//    video.title = dictionary["title"] as? String
+//    video.thumbnailImageName = dictionary["thumbnail_image_name"] as? String
+//
+//    let channelDictionary = dictionary["channel"] as! [String: AnyObject]
+//
+//    let channel = Channel()
+//    channel.name = channelDictionary["name"] as? String
+//    channel.profileImageName = channelDictionary["profile_image_name"] as? String
+//
+//    video.channel = channel
+//
+//    videos.append(video)
+//
+//}
+//
+//DispatchQueue.main.async {
+//    completion(videos)
+//
+//}
 
 
